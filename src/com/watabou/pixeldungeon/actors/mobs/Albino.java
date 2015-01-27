905f7fcc2cbd8d150c3ae60dc29f892ca67aae96
Schuyler Harris <schuylernathaniel@users.noreply.github.com>
2015-01-27 13:37:09 -0800

    Update Albino.java

--- src/com/watabou/pixeldungeon/actors/mobs/Albino.java
+++ src/com/watabou/pixeldungeon/actors/mobs/Albino.java
@@ -22,7 +22,7 @@ import com.watabou.pixeldungeon.actors.Char;
 import com.watabou.pixeldungeon.actors.buffs.Bleeding;
 import com.watabou.pixeldungeon.actors.buffs.Buff;
 import com.watabou.pixeldungeon.sprites.AlbinoSprite;
-import com.watabou.utils.Random;
+import com.watabou.pixeldungeon.utils.Random;
 
 public class Albino extends Rat {
 
