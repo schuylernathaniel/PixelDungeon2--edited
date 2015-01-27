6e8f539e5f039baae84ec097147a51ae52ebccc3
Schuyler Harris <schuylernathaniel@users.noreply.github.com>
2015-01-27 13:47:59 -0800

    Update Statue.java

--- src/com/watabou/pixeldungeon/actors/mobs/Statue.java
+++ src/com/watabou/pixeldungeon/actors/mobs/Statue.java
@@ -32,8 +32,8 @@ import com.watabou.pixeldungeon.items.weapon.enchantments.Death;
 import com.watabou.pixeldungeon.items.weapon.enchantments.Leech;
 import com.watabou.pixeldungeon.items.weapon.melee.MeleeWeapon;
 import com.watabou.pixeldungeon.sprites.StatueSprite;
-import com.watabou.utils.Bundle;
-import com.watabou.utils.Random;
+import com.watabou.pixeldungeon.utils.Bundle;
+import com.watabou.pixeldungeon.utils.Random;
 
 public class Statue extends Mob {
 	
@@ -41,7 +41,7 @@ public class Statue extends Mob {
 		name = "animated statue";
 		spriteClass = StatueSprite.class;
 
-		EXP = 0;
+		EXP = 500;
 		state = PASSIVE;
 	}
 	
@@ -52,13 +52,13 @@ public class Statue extends Mob {
 		
 		do {
 			weapon = (Weapon)Generator.random( Generator.Category.WEAPON );
-		} while (!(weapon instanceof MeleeWeapon) || weapon.level < 0);
+		} while (!(weapon instanceof MeleeWeapon) && weapon.level < 0);
 		
 		weapon.identify();
 		weapon.enchant( Enchantment.random() );
 		
-		HP = HT = 15 + Dungeon.depth * 5;
-		defenseSkill = 4 + Dungeon.depth;
+		HP = HT = Dungeon.depth;
+		defenseSkill = Dungeon.depth;
 	}
 	
 	private static final String WEAPON	= "weapon";
@@ -90,12 +90,12 @@ public class Statue extends Mob {
 	
 	@Override
 	public int attackSkill( Char target ) {
-		return (int)((9 + Dungeon.depth) * weapon.ACU);
+		return (int)(( Dungeon.depth) * weapon.ACU -5);
 	}
 	
 	@Override
 	protected float attackDelay() {
-		return weapon.DLY;
+		return weapon.DLY + 5f;
 	}
 	
 	@Override
@@ -110,7 +110,7 @@ public class Statue extends Mob {
 			state = HUNTING;
 		}
 		
-		super.damage( dmg, src );
+		super.damage( dmg + 5, src );
 	}
 	
 	@Override
@@ -151,11 +151,6 @@ public class Statue extends Mob {
 	private static final HashSet<Class<?>> RESISTANCES = new HashSet<Class<?>>();
 	private static final HashSet<Class<?>> IMMUNITIES = new HashSet<Class<?>>();
 	static {
-		RESISTANCES.add( ToxicGas.class );
-		RESISTANCES.add( Poison.class );
-		RESISTANCES.add( Death.class );
-		RESISTANCES.add( ScrollOfPsionicBlast.class );
-		IMMUNITIES.add( Leech.class );
 	}
 	
 	@Override
