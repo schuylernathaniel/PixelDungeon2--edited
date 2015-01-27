1a0fdd5f414ef88a9f73e4e90b18cfae47e71baa
Schuyler Harris <schuylernathaniel@users.noreply.github.com>
2015-01-27 13:14:54 -0800

    Update Bleeding.java

--- src/com/watabou/pixeldungeon/actors/buffs/Bleeding.java
+++ src/com/watabou/pixeldungeon/actors/buffs/Bleeding.java
@@ -23,9 +23,9 @@ import com.watabou.pixeldungeon.effects.Splash;
 import com.watabou.pixeldungeon.ui.BuffIndicator;
 import com.watabou.pixeldungeon.utils.GLog;
 import com.watabou.pixeldungeon.utils.Utils;
-import com.watabou.utils.Bundle;
-import com.watabou.utils.PointF;
-import com.watabou.utils.Random;
+import com.watabou.pixeldungeon.utils.Bundle;
+import com.watabou.pixeldungeon.utils.PointF;
+import com.watabou.pixeldungeon.utils.Random;
 
 public class Bleeding extends Buff {
 	
